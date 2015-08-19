fe4135cb64f07936389e5751e1faa02ace50a6c4
xerxesv <xerxesv@gmail.com>
2015-08-19 14:52:47 -0700

    Wrote out functional queue, but tests dont pass

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,37 @@
 var Queue = function(){
   var someInstance = {};
-
+  
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+	storage[counter] = value;
+	counter++;
   };
 
   someInstance.dequeue = function(){
+
+	for (var i = 1; i <= counter; i++){ //storage.length is unknown at this point
+		storage[i-1] = storage[i];
+	}
+	delete storage[counter];
+	counter--;
   };
 
   someInstance.size = function(){
+	return counter + 1;
   };
 
   return someInstance;
 };
+
+/*
+var myQueue = Queue();
+
+myQueue.enqueue("firstthing");
+
+*/
+//myQueue.storage === {0: "firstthing"}
