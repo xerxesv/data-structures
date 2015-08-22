2986ff83a13e0f3e0ea058d40da0fc76e5529688
Hack Reactor Students <accounts@hackreactor.com>
2015-08-21 21:32:36 -0700

    Work on Hash remove method, other tests pass

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,32 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i,[k,v]);
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i)[1];
+
 
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i,null);
 
+  // this._storage.each(function(tupal,index){
+  //   if(index===i){
+  //     this.set(i,undefined)
+  //   }
+  // })
+
+  // this._storage.each(function(tupal, index) {
+  //   if (index === i) {
+  //      = undefined;
+  //   }
+  // })
 };
 
 
