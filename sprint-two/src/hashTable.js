3a56b15300ecb1fb8c38d4bf5aa82c4a49b20316
Hack Reactor Students <accounts@hackreactor.com>
2015-08-22 00:23:32 -0700

    work on fixing collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,24 +1,55 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-};
 
+};
+var bucket;
+var tupal;
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i,[k,v]);
 
+
+  if (!this._storage.get(i)) {
+    bucket = [];
+    tupal = [k, v];
+
+    bucket.push(tupal);
+    this._storage.set(i, bucket)
+  } else {
+    bucket.pupsh([k, v]);
+  }
+
+
+
+
+  //   //loop over tupal(s)
+  //   for (var j = 0; j < this._storage.get(i).length; j++) {
+  //     //check if existing tupal's key is equal to new key being inserted
+  //     if (this._storage.get(i)[j][0] === k) {
+  //       //if so store existing key's value into variable
+  //       oldValue = this._storage.get(i)[1];
+  //       this._storage.get(i)[j][1] = v; 
+  //     } else {
+  //       this._storage.get(i).push([k, v]);
+  //     }
+  //   }
+  // } else {
+  // this._storage.get(i).push([k,v]);
+  // }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(i) === null) {
+   return null;
+  }
   return this._storage.get(i)[1];
-
 
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i,null);
+  this._storage.set(i, null);
 
   // this._storage.each(function(tupal,index){
   //   if(index===i){
