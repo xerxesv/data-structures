41c60e21b58a037b93d2a135da0e3ea77552099c
Hack Reactor Students <accounts@hackreactor.com>
2015-08-22 13:32:45 -0700

    insert on binarySearchTree works

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,43 @@
 var BinarySearchTree = function(value){
+  treeInstance = {};
+
+  treeInstance.value = value;
+  treeInstance.left = null;
+  treeInstance.right = null;
+
+  _.extend(treeInstance, binarySearchTreeMethods);
+
+  return treeInstance;
+};
+
+var binarySearchTreeMethods = {
+
+  insert: function(value) {
+    
+    if (this.left === null) {
+      //if value of new node inserted is less than this.value
+      if (value < this.value) {
+        this.left = BinarySearchTree(value);
+      }
+    } else {
+      this.left.insert(value);
+    }
+
+    if (this.right === null) {
+      if (value > this.value) {
+        this.right = BinarySearchTree(value);
+      }
+    } else {
+      this.right.insert(value);
+    }
+   
+
+  },
+
+  contains: function() {},
+
+  depthFirstLog: function() {}
+
 };
 
 
